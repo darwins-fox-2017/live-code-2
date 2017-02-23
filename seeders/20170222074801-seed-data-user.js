5739488117711682b387bca2ab1d36f6b0d9787a
Windi <windi@hacktiv8.com>
2017-02-23 13:05:35 +0700

    repo

---
+++ seeders/20170222074801-seed-data-user.js
@@ -0,0 +1,35 @@
+'use strict';
+var db = require('../models');
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+    return db.Category.bulkCreate([
+        { name: "Sport" },
+        { name: "Food" },
+        { name: "Style" },
+        { name: "Sale" },
+    ], {})
+    
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
