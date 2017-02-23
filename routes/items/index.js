000f7769a551f1b3a5108c5868ae73757e0319f2
Daniel Sidabutar <email": null>
2017-02-23 14:23:50 +0700

    ez

--- routes/items/index.js
+++ routes/items/index.js
@@ -4,13 +4,17 @@ var models = require('../../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  models.Item.findAll().then(function(items) {
+  models.Item.findAll({
+    include: [{model: models.Category}]
+  }).then(function(items) {
     res.render('items/index', { title: 'Test', items: items });
   })
 });
 
 router.get('/add', function(req, res, next) {
-  res.render('items/add', { title: 'Express' });
+  models.Category.findAll().then(function(cat) {
+    res.render('items/add', { title: 'Express', category: cat });
+  })
 });
 
 router.post('/add', function(req, res, next) {
@@ -23,9 +27,29 @@ router.post('/add', function(req, res, next) {
   })
 })
 
-router.get('edit/:id', function(req, res, next) {
-  models.Item.findOne(req.params.id).then(function(item) {
-    res.redirect('items/edit')
+router.get('/delete/:id', function(req, res, next) {
+  models.Item.destroy({
+    where: {id: req.params.id}
+  }).then(function() {
+    res.redirect('/items')
+  })
+})
+
+router.get('/edit/:id', function(req, res, next) {
+  models.Item.findById(req.params.id).then(function(item) {
+    models.Category.findAll().then(function(category) {
+      res.render('items/edit', {category: category, item: item})
+    })
+  })
+})
+
+router.post('/update/:id', function(req, res, next) {
+  models.Item.update({
+    name: req.body.name,
+    quantity: req.body.quantity,
+    CategoryId: req.body.CategoryId,
+  }, {where: {id: req.params.id}}).then(function() {
+    res.redirect('/items')
   })
 })
 
